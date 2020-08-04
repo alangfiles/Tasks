f5bfdec7bc9212bdadf7acef8cd8d146cadb419d
Alan Files <alangfiles@users.noreply.github.com>
2020-08-04 17:10:18 -0400

    build folder

---
+++ build/precache-manifest.01f6aacf4ef45dd9ea389bb1f0bc0003.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c07402faf61268673d83b91a384075c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a22398d8fa0d5402dcc3",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1a57beac83295a3e2a3f",
+    "url": "/static/js/2.8a57fc52.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8a57fc52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a22398d8fa0d5402dcc3",
+    "url": "/static/js/main.df54eb43.chunk.js"
+  },
+  {
+    "revision": "142af1de3a19a148c46f",
+    "url": "/static/js/runtime-main.0c6910c8.js"
+  }
+]);
